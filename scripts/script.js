e4f9dc72a35e92f06f6c5956a433cb5537b69163
jmartinelena <jmartinelena1@gmail.com>
2022-10-16 22:00:56 -0300

    Se agrega estructura basica funcional de la pagina

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+function checkMenu() {
+    if (document.getElementById("menu-check").checked) {
+        document.getElementById("navbar").style.display = "none";
+        document.getElementById("menu-cerrar").style.display = "none";
+        document.getElementById("menu-abrir").style.display = "inline";
+    }
+    else 
+    {
+        document.getElementById("navbar").style.display = "grid";
+        document.getElementById("menu-cerrar").style.display = "inline";
+        document.getElementById("menu-abrir").style.display = "none";
+    }  
+}
+
+function mostrarNavbar() {
+    let ancho = document.documentElement.clientWidth;
+    console.log(ancho);
+    if (ancho <= 600) {
+        document.getElementById("menu-cerrar").style.display = "none";
+        document.getElementById("menu-abrir").style.display = "inline";
+        document.getElementById("navbar").style.display = "none";
+        document.getElementById("menu-check").checked = true;
+    }
+    if (ancho >600) {
+        document.getElementById("menu-cerrar").style.display = "inline";
+        document.getElementById("menu-abrir").style.display = "none";
+        document.getElementById("navbar").style.display = "grid";
+        document.getElementById("menu-check").checked = false;
+    }
+}
+
+window.addEventListener('resize', mostrarNavbar);
